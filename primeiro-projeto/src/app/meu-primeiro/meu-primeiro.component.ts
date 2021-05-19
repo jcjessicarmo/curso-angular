c5204d049c51c0e0f097d486d866235bb8b15142
jcjessicarmo <jessicascarmo@terra.com.br>
2021-05-19 10:50:43 -0300

    Criando meu primeiro componente manualmente com angular

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component( {
+    selector: 'meu-primeiro-component',
+    template: `
+    <p> Meu primeiro componente criado com Angular! <p>
+    `
+
+})
+export class MeuPrimeiroComponent { }
