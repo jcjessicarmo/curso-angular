25e6243d7921e8e14b69236373cc52a5489857dc
jcjessicarmo <jessicascarmo@terra.com.br>
2021-05-21 09:41:24 -0300

    Aula 07 - Injeção de dependência

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CursosService} from './cursos.service';
 
 @Component({
   selector: 'app-cursos',
@@ -9,15 +10,18 @@ export class CursosComponent implements OnInit {
 
   nomePortal: string;
 
-  cursos: string[] = ['Java,', 'Ext JS', 'Angular'];
+  cursos: string[];
 
-  constructor() {
+  constructor(private cursosService: CursosService) {
     this.nomePortal = 'http//:loiane.training';
 
 
-    for (let i=0; i<this.cursos.length; i++){
+   /*for (let i=0; i<this.cursos.length; i++){
       let curso = this.cursos[i];
-    }
+    }*/
+
+    //var servico = new CursosService(); 
+    this.cursos = this.cursosService.getCursos();
 
   }
 
