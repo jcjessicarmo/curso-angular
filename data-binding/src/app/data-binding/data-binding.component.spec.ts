f9c7849a688439af170fb7c1e57ad777cdf45f27
jcjessicarmo <jessicascarmo@terra.com.br>
2021-08-31 19:47:46 -0300

    Aula 10 - Classe e Style binding

--- data-binding/src/app/data-binding/data-binding.component.spec.ts
+++ data-binding/src/app/data-binding/data-binding.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DataBindingComponent } from './data-binding.component';
 
-describe('DataBindingComponent', debugger() => {
+describe('DataBindingComponent', () => {
   let component: DataBindingComponent;
   let fixture: ComponentFixture<DataBindingComponent>;
 
